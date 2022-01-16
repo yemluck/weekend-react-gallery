833e1f4a708b7fc7bb6a8dab10b666e19467555b
Chris Agbolade <yemluck@gmail.com>
2022-01-16 01:24:31 -0600

    axios getting response from data. Response saved as galleryList

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header (){
+return (
+<>
+        <header className="App-header">
+            <h1 className="App-title">Gallery of My Life new component</h1>
+        </header>
+
+</>
+)
+}
+
+export default Header
